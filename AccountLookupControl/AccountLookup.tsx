d48fd3959b0542b3c963a2de6349851008535f7f
Jon W <90464677+codecarveruk@users.noreply.github.com>
2025-06-02 15:22:26 +0100

    Implement AccountLookup component with ComboBox for account selection

--- AccountLookupControl/AccountLookup.tsx
+++ AccountLookupControl/AccountLookup.tsx
@@ -1,7 +1,21 @@
 import * as React from "react";
+import { ComboBox, IComboBoxOption } from "@fluentui/react";
+
+const options: IComboBoxOption[] = [
+    { key: '1', text: 'Account One' },
+    { key: '2', text: 'Account Two' },
+    { key: '3', text: 'Account Three' }
+];
 
 const AccountLookup: React.FC = () => {
-    return <div>Account Lookup Placeholder</div>;
+    return (
+        <ComboBox
+            label="Account"
+            options={options}
+            placeholder="Select an account..."
+            style={{ minWidth: 250 }}
+        />
+    );
 };
 
 export default AccountLookup;
