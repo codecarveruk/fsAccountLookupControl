dcbad1f5c3015bf9a2d66d730bd411048d727ca5
Jon W <90464677+codecarveruk@users.noreply.github.com>
2025-06-02 15:32:25 +0100

    Add filtering functionality to AccountLookup component for dynamic account search

--- AccountLookupControl/AccountLookup.tsx
+++ AccountLookupControl/AccountLookup.tsx
@@ -36,12 +36,30 @@ const AccountLookup: React.FC<AccountLookupProps> = ({ selectedKey, onChange, co
         onChange(option ? option.key : undefined);
     };
 
+    const handleFilter = async (_event: React.FormEvent<IComboBox>, option?: IComboBoxOption, index?: number, value?: string) => {
+        const filter = value ? `&$filter=contains(name,'${value.replace(/'/g, "''")}')` : '';
+        try {
+            const result = await context.webAPI.retrieveMultipleRecords(
+                "account",
+                `$select=accountid,name&$top=10${filter}`
+            );
+            const opts = (result.entities as AccountEntity[]).map((a) => ({
+                key: a.accountid,
+                text: a.name
+            }));
+            setOptions(opts);
+        } catch {
+            setOptions([]);
+        }
+    };
+
     return (
         <ComboBox
             label="Account"
             options={options}
             selectedKey={selectedKey}
             onChange={handleChange}
+            onInput={(event: React.FormEvent<IComboBox>, option?: IComboBoxOption, index?: number, value?: string) => { void handleFilter(event, option, index, value); }}
             placeholder="Select an account..."
             style={{ minWidth: 250 }}
         />
